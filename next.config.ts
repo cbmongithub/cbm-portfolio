674576985aaadc92445292cb059dcdbaef4d7a6c
Your Name <you@personal.com>
2025-12-13 08:58:46 -0700

    chore: create logo.tsx file for reusage of logo later

--- next.config.ts
+++ next.config.ts
@@ -20,9 +20,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  experimental: {
-    turbopackFileSystemCacheForDev: true,
-  },
 };
 
 export default nextConfig;
---
+++ src/components/ui/logo.tsx
@@ -0,0 +1,22 @@
+export function Logo() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="300.047"
+      height="171.5"
+      preserveAspectRatio="none"
+      viewBox="99.974 164.102 300.047 171.5"
+      className="h-6 w-full max-w-[41.82px]"
+      aria-hidden="true"
+    >
+      <title>Cbm Logo</title>
+      <text
+        x="99.974"
+        y="303.102"
+        className="fill-foreground hover:fill-muted-foreground text-[153.4px] transition-colors"
+      >
+        𝕔𝕓𝕞
+      </text>
+    </svg>
+  );
+}
